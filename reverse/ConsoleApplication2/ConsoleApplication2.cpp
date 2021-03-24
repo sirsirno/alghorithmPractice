b253f1df3573bc8d309dc6114e41ecccf0fca50a
sirsirno <wahybzb24@gmail.com>
2021-03-24 11:45:00 +0900

    키킼 넷마블

---
+++ reverse/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿
+//20208 서선호 
+#include <iostream>
+using namespace std;
+
+
+void Print(char arr[])
+{
+    int counter, length = 0;
+    while (arr[length] != 0) length++;
+
+    for (counter = length - 1; counter >= 0; counter--) {
+        cout << arr[counter];
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    
+    char word[3][10] = { 0, };
+    for (int counter = 0; counter < 3; counter++)
+    {
+        cout << "문자열을 입력해주십시오 최대 10글자." <<"( "<< counter << "/ 3 )" << "\n";
+        cin >> word[counter];
+    }
+    Print(word[0]);
+    Print(word[1]);
+    Print(word[2]);
+}
---
+++ samgakhyeong/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,25 @@
+﻿// 20208 서선호
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int arr[10][10] = { 0, };
+	int value1, value2;
+
+	for (value1 = 0; value1 < 10; value1++) {
+		arr[value1][0] = 1;
+	}
+	for (value1 = 0; value1 < 10; value1++) {
+		for (value2 = 0; value2 < 10; value2++) {
+			if (value2 > 0 && value1 > 0) {
+				arr[value1][value2] = arr[value1 - 1][value2] + arr[value1 - 1][value2 - 1];
+				if (arr[value1][value2] > 0)
+					cout << arr[value1][value2] << " ";
+			}
+	
+		}
+		cout << endl;
+}
+	return 0;
+}
